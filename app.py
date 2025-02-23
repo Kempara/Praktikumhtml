dff53f7f52911ed9e9b684facefcdafa00bcaed5
Kempara <radiglobul@gmail.com>
2025-02-23 11:40:05 +0200

    recently changes

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template('Vag.html')
 
+@app.route('/reservation')
+def reservation():
+    return render_template('reservation.html')
+
+@app.route('/program')
+def program():
+    return render_template('programa.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
